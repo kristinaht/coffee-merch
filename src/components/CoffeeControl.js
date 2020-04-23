4584e2eb478b3ce3a2a114ad24ba809453e2843b
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-23 13:47:59 -0700

    add sceleton code to CoffeeControl.js

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import CoffeeList from './CoffeeList';
 import NewCoffeeForm from './NewCoffeeForm';
+
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}; 
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default CoffeeControl;
