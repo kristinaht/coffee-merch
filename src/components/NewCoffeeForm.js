6858d49aaab8e6c5d0b72d99d2e9eda3e197cebb
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-23 15:41:20 -0700

    add form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
 function NewCoffeeForm(props){
   return(
     <React.Fragment>
-      <h2>Imagine a form here.</h2>
+      <form onSubmit={handleNewCoffeeFormSubmission}>
+        <input
+        type='text'
+        name='name'
+        placeholder='Coffee drink name' />
+        <input
+        type='text'
+        description='description'
+        placeholder='Description' />
+      </form>
     </React.Fragment>
   );
 }
