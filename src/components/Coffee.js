be3c862ff582611b3bdcff7b94da6b65000535bc
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-23 13:21:24 -0700

    create initial Coffee.js component

---
+++ src/components/Coffee.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Coffee(props){
+  return(
+    <p>espresso</p>
+  );
+}
+
+export default Coffee;
+
+Coffee.propTypes = {
+  
+}
